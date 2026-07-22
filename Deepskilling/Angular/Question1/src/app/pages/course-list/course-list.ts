6e2e7d186e82cd7f0ba8aa3e3fa2653b01125d37
Priya Koripuri <purnakoripuri@gmail.com>
2026-07-22 11:32:24 +0530

    Add Angular Question 1 student profile

---
+++ Deepskilling/Angular/Question1/src/app/pages/course-list/course-list.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-course-list',
+  imports: [],
+  templateUrl: './course-list.html',
+  styleUrl: './course-list.css'
+})
+export class CourseList {
+}
